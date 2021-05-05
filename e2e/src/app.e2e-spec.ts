edfd27e1a59c347e603196b33f10baef61617bad
raaedk <raaedk@gmail.com>
2021-05-05 16:25:42 -0400

    formatted files

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -11,17 +11,20 @@ describe('workspace-project App', () => {
   it('should display welcome message', async () => {
     await page.navigateTo();
     expect(await page.getTitleText()).toEqual(
-      'carDealerClient app is running!'
+      'carDealerClient app is running!',
     );
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    const logs = await browser
+      .manage()
+      .logs()
+      .get(logging.Type.BROWSER);
     expect(logs).not.toContain(
       jasmine.objectContaining({
         level: logging.Level.SEVERE,
-      } as logging.Entry)
+      } as logging.Entry),
     );
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'carDealerClient';
+
+  public test = 'hello';
+
+  constructor() {
+    this.test = '';
+  }
 }
