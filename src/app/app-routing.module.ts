1869904442ed381ccc1d867d5fab38782bd310a8
Robert Muehler <robertmuehler@gmail.com>
2021-05-06 10:57:51 -0400

    added routing, ngFor displaying vehicle components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VehicleListComponent } from './components/vehicle-list/vehicle-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: VehicleListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
