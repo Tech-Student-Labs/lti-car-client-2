b4501a8e1aeed25d4cda15b4a31f7475ec692698
Robert Muehler <robertmuehler@gmail.com>
2021-05-06 11:29:59 -0400

    Revert "Feature/inventory service"

--- src/app/models/vehicle.ts
+++ src/app/models/vehicle.ts
@@ -1,14 +1,3 @@
-export default class Vehicle {
-  make: string;
-  model: string;
-  year: number;
-  miles: number;
-  color: string;
-  images: string[];
-  vin: string;
-  offerPrice: number;
-  sellingPrice: number;
-  seller: number;
-
-  constructor() {}
+export class Vehicle {
+  //TODO: fill this out with vehicle info
 }
