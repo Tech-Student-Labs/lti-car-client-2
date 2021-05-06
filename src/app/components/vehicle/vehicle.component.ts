c5dba266df6d0dc5d72b6cd4d08b3886e5872ef7
Andrew Mitschke <ammitchky@gmail.com>
2021-05-06 11:09:42 -0500

    added basic display for vehicle data

--- src/app/components/vehicle/vehicle.component.ts
+++ src/app/components/vehicle/vehicle.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Vehicle } from 'src/app/models/vehicle';
 
 @Component({
   selector: 'app-vehicle',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle.component.css'],
 })
 export class VehicleComponent implements OnInit {
+  @Input() vehicle: Vehicle;
+
   constructor() {}
 
   ngOnInit(): void {}
