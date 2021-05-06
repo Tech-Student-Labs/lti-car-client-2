0138321465970a979e562d50fcf3470d1a5e47c5
Robert Muehler <robertmuehler@gmail.com>
2021-05-06 10:00:39 -0400

    created Inventory module, added Veh and VehList comp, strucutre of Veh model.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
+import { InventoryModule } from './modules/inventory/inventory.module';
+import { VehicleListComponent } from './components/vehicle-list/vehicle-list.component';
+import { VehicleComponent } from './components/vehicle/vehicle.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, InventoryModule],
   providers: [],
   bootstrap: [AppComponent],
 })
