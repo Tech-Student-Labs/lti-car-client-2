71f4746366e29d9e056e86442620b86c128c89b2
raaedk <raaedk@gmail.com>
2021-05-06 15:14:47 -0400

    seperated mock from service spec

--- src/app/services/inventory.service.ts
+++ src/app/services/inventory.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { InventoryServiceAbstract } from './InventoryAbstract';
+import Vehicle from '../models/vehicle';
 
 @Injectable({
   providedIn: 'root',
@@ -11,16 +14,13 @@ InventoryService
 Handles data of the inventory. Currently most implementation is commented out to allow mock data from abstract class to be used.
 */
 export class InventoryService extends InventoryServiceAbstract {
-  // public vehicles: Vehicle[];
-
-  // public endpoint: string = '';
+  public endpoint: string = '';
 
   constructor(private http: HttpClient) {
     super();
   }
 
-  //Commented out so the service uses mock data from the abstract
-
+  // Commented out so the service uses mock data from the abstract
   // getAllVehicles(): Observable<Vehicle[]> {
   //   return this.http.get<Vehicle[]>(this.endpoint).pipe(
   //     tap((data) => {
--- src/app/tests/inventory.service.spec.ts
+++ src/app/tests/inventory.service.spec.ts
@@ -5,6 +5,7 @@ import vehicles from './data/vehicles.json';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import Vehicle, { convertVehicle } from '../models/vehicle';
 import { of } from 'rxjs';
+import { InventoryServiceAbstract } from '../services/InventoryAbstract';
 
 const TOYOTA_VIN = '123';
 
@@ -32,7 +33,7 @@ describe('InventoryService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [],
+      providers: [InventoryServiceAbstract],
     });
     service = TestBed.inject(InventoryService);
     httpService = TestBed.inject(HttpClient);
@@ -42,57 +43,10 @@ describe('InventoryService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('getAll() should get all vehicles in inventory list', () => {
+  it('httpget the mock vehicles should be functional', () => {
     spyOn(httpService, 'get').and.returnValue(of(mockVehicles));
     service.getAllVehicles().subscribe((data) => {
-      expect(data.length).toBe(mockVehicles.length);
-    });
-  });
-
-  it('mock has default values', () => {
-    expect(service.vehicles.length).toBeGreaterThan(0);
-    expect(service.vehicles).toEqual(mockVehicles);
-  });
-
-  it('getByVIN() should get one vehicle by searching for its vin number', () => {
-    expect(service).toBeTruthy();
-    service.getByVIN(TOYOTA_VIN).subscribe((data) => {
-      expect(data).toBeDefined();
-      expect(data.model).toBe('Corolla');
-    });
-  });
-
-  it('changeSalePrice() should change the sale price of a vehicle', () => {
-    const newPrice = 2500;
-    expect(service).toBeTruthy();
-    service.getByVIN(TOYOTA_VIN).subscribe((data) => {
-      expect(data.sellingPrice).not.toBe(newPrice);
-    });
-    service.changeSalePrice(TOYOTA_VIN, 2500);
-    service.getByVIN(TOYOTA_VIN).subscribe((data) => {
-      expect(data.sellingPrice).toBe(newPrice);
-    });
-  });
-
-  it('removeVehicle() should delete the vehicle from inventory', fakeAsync(() => {
-    //check if vehicle exists in inventory
-    service.getByVIN(TOYOTA_VIN).subscribe((data) => {
-      expect(data).toBeDefined();
-    });
-    service.removeVehicle(TOYOTA_VIN);
-    tick();
-    service.getByVIN(TOYOTA_VIN).subscribe((data) => {
-      expect(data).not.toBeDefined();
-    });
-  }));
-
-  it('addVehicle() should add a vehicle to the inventory', () => {
-    service.getByVIN('666').subscribe((data) => {
-      expect(data).not.toBeDefined();
-    });
-    service.addVehicle(TEST_VEHICLE_JSON);
-    service.getByVIN('666').subscribe((data) => {
-      expect(data).toBeDefined();
+      expect(data).toEqual(mockVehicles);
     });
   });
 });
