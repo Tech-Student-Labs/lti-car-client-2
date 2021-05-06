8fa7a0e753bad7a06ef668c9d91aca90c59ee3fd
raaedk <raaedk@gmail.com>
2021-05-06 11:10:50 -0400

    cleaned up abstract implemenation

--- src/app/services/InventoryAbstract.ts
+++ src/app/services/InventoryAbstract.ts
@@ -3,7 +3,9 @@ import vehicles from '../tests/data/vehicles.json';
 import Vehicle from '../models/vehicle';
 
 export abstract class InventoryServiceAbstract {
-  public vehicles: Vehicle[] = [...vehicles];
+  public vehicles: Vehicle[] = vehicles.map((data) =>
+    Object.assign(new Vehicle(), data),
+  );
 
   public getAllVehicles(): Observable<Vehicle[]> {
     return of(this.vehicles);
