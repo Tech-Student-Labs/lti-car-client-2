55461a7e1cdd16a6f0be44fe868d6f5b1d53b87e
raaedk <raaedk@gmail.com>
2021-05-07 11:20:46 -0400

    added submission form to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { SubmissionFormComponent } from './components/submission-form/submission-form.component';
 import { VehicleListComponent } from './components/vehicle-list/vehicle-list.component';
 
 const routes: Routes = [
   { path: '', component: VehicleListComponent },
   { path: 'about-us', component: AboutUsComponent },
+  { path: 'submission', component: SubmissionFormComponent },
 ];
 
 @NgModule({
