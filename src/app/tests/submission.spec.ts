3d9691ae11f2582fe900bba26af929fa20571abf
raaedk <raaedk@gmail.com>
2021-05-07 13:16:22 -0400

    submissions model created

--- src/app/tests/submission.spec.ts
+++ src/app/tests/submission.spec.ts
@@ -1,3 +1,4 @@
+import { single } from 'rxjs/operators';
 import Submission, { Status } from '../models/submission';
 import Vehicle, { convertVehicle } from '../models/vehicle';
 import vehicles from '../tests/data/vehicles.json';
@@ -8,13 +9,22 @@ describe('Vehicle', () => {
 
   beforeEach(() => {
     vehicle = convertVehicle(vehicles[0]);
+    submission = new Submission('1', vehicle);
   });
 
   it('should create an instance', () => {
-    expect(new Submission('1', vehicle)).toBeTruthy();
+    expect(submission).toBeTruthy();
   });
 
   it('status should be pending', () => {
-    expect();
+    expect(submission.isPending()).toBeTruthy();
+  });
+
+  it('decline and accept submissions', () => {
+    submission.acceptSubmission();
+    expect(submission.status).toBe(Status.Accepted);
+
+    submission.declineSubmission();
+    expect(submission.status).toBe(Status.Declined);
   });
 });
