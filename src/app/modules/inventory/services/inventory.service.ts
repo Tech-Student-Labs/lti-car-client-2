e853a2a4961bcf37cef8ea408582ddab9c0e4fe6
Andrew Mitschke <ammitchky@gmail.com>
2021-06-10 14:50:00 -0500

    added basic login form with token setup

--- src/app/modules/inventory/services/inventory.service.ts
+++ src/app/modules/inventory/services/inventory.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { InventoryServiceAbstract } from './InventoryAbstract';
-import InventoryVehicle from '../../../models/inventory-vehicle';
+import Vehicle from '../../../models/vehicle';
 
 @Injectable({
   providedIn: 'root',
@@ -14,24 +14,21 @@ InventoryService
 Handles data of the inventory. Currently most implementation is commented out to allow mock data from abstract class to be used.
 */
 export class InventoryService extends InventoryServiceAbstract {
-  public endpoint = 'https://localhost:5001/Inventory';
+  public endpoint: string = 'https://localhost:5001/Vehicle';
 
   constructor(private http: HttpClient) {
     super();
   }
 
   // Commented out so the service uses mock data from the abstract
-  getAllVehicles(): Observable<InventoryVehicle[]> {
-    return this.http
-      .get<InventoryVehicle[]>(this.endpoint, {
-        headers: new HttpHeaders({
-          'Content-Type': 'application/json',
-        }),
+  getAllVehicles(): Observable<Vehicle[]> {
+    return this.http.get<Vehicle[]>(this.endpoint, {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })}).pipe(
+      tap((data) => {
+        this.vehicles = data;
       })
-      .pipe(
-        tap((data) => {
-          this.vehicles = data;
-        }),
-      );
+    );
   }
 }
