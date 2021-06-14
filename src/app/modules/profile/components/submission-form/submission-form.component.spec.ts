8445ade226bc8bd213b3f472045c260e2a32c96a
rmuehler <67pha0vz!!!!!>
2021-06-14 16:35:19 -0400

    test for checking if form renders, doesnt pass

--- src/app/modules/profile/components/submission-form/submission-form.component.spec.ts
+++ src/app/modules/profile/components/submission-form/submission-form.component.spec.ts
@@ -5,6 +5,7 @@ import { SubmissionFormComponent } from './submission-form.component';
 describe('SubmissionFormComponent', () => {
   let component: SubmissionFormComponent;
   let fixture: ComponentFixture<SubmissionFormComponent>;
+  let htmlElement: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,10 +16,35 @@ describe('SubmissionFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SubmissionFormComponent);
     component = fixture.componentInstance;
+    htmlElement = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should render vehicle submission forms', () => {
+    expect(
+      htmlElement.querySelector('#make-submission'),
+    ).toBeTruthy();
+    expect(
+      htmlElement.querySelector('#model-submission'),
+    ).toBeTruthy();
+    expect(
+      htmlElement.querySelector('#year-submission'),
+    ).toBeTruthy();
+    expect(htmlElement.querySelector('#vin-submission')).toBeTruthy();
+    expect(
+      htmlElement.querySelector('#miles-submission'),
+    ).toBeTruthy();
+    expect(
+      htmlElement.querySelector('#color-submission'),
+    ).toBeTruthy();
+    expect(
+      htmlElement.querySelector('#images-submission'),
+    ).toBeTruthy();
+    expect(
+      htmlElement.querySelector('#price-submission'),
+    ).toBeTruthy();
   });
 });
--- src/app/modules/profile/components/submission-form/submission-form.component.ts
+++ src/app/modules/profile/components/submission-form/submission-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-submission-form',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./submission-form.component.css'],
 })
 export class SubmissionFormComponent implements OnInit {
-  constructor() {}
+  submissionForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {}
 }
