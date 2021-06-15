692e9676c5d6790d8d7b03efbb064e3dfdbbcca3
rmuehler <67pha0vz!!!!!>
2021-06-15 13:41:45 -0400

    get form data, convert image to base64

--- src/app/modules/profile/components/submission-form/submission-form.component.ts
+++ src/app/modules/profile/components/submission-form/submission-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Vehicle from '../../../../models/vehicle';
+import { VehicleSubmissionService } from '../../services/vehicle-submission.service';
+import { resolve } from '@angular/compiler-cli/src/ngtsc/file_system';
+import { errors } from 'puppeteer';
+import VehicleImage from '../../../../models/vehicle-image';
 
 @Component({
   selector: 'app-submission-form',
@@ -8,8 +13,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SubmissionFormComponent implements OnInit {
   submissionForm: FormGroup;
+  vehicle: Vehicle;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private submissionService: VehicleSubmissionService,
+  ) {}
 
   ngOnInit(): void {
     this.submissionForm = this.formBuilder.group({
@@ -22,5 +31,39 @@ export class SubmissionFormComponent implements OnInit {
       images: ['', Validators.required],
       price: ['', Validators.required],
     });
+    this.vehicle = new Vehicle();
   }
+
+  public onSubmit(form: any): void {
+    if (form.valid === false) {
+      alert('Please fill out all vehicle information.');
+    }
+    const formData = form.value;
+    this.vehicle.make = formData.make;
+    this.vehicle.model = formData.model;
+    this.vehicle.year = +formData.year;
+    this.vehicle.vin = formData.vin;
+    this.vehicle.miles = formData.miles;
+    this.vehicle.color = formData.color;
+    this.vehicle.sellingPrice = formData.price;
+  }
+
+  processImages = (imagesInput: FileList) => {
+    Array.from(imagesInput).forEach((file) => {
+      const vehicleImage = new VehicleImage();
+      this.imageToBase64(file).then((data: string) => {
+        vehicleImage.imageData = data;
+        this.vehicle.vehicleImages.push(vehicleImage);
+        console.log(vehicleImage.imageData);
+      });
+    });
+  };
+
+  imageToBase64 = (file: File) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
 }
--- src/app/modules/profile/services/vehicle-submission.service.ts
+++ src/app/modules/profile/services/vehicle-submission.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import Vehicle from '../../../models/vehicle';
 import { Observable, of } from 'rxjs';
 import Submission from '../../../models/submission';
+import VehicleImage from '../../../models/vehicle-image';
 
 @Injectable({
   providedIn: 'root',
 })
 export class VehicleSubmissionService {
   vehicleSubmissions: Submission[] = [];
+
   constructor() {}
 
   addSubmission(vehicle: Vehicle): void {
@@ -46,5 +48,20 @@ export class VehicleSubmissionService {
 
   getAll(): Observable<Submission[]> {
     return of(this.vehicleSubmissions);
+  }
+
+  convertVehicleImagesToString(files): VehicleImage[] {
+    // tslint:disable-next-line:prefer-const
+    let images: VehicleImage[];
+    const fileReader = new FileReader();
+    // for (let i = 0; i < files.length; i++) {
+    // tslint:disable-next-line:prefer-const
+    let vehicleImage = new VehicleImage();
+    vehicleImage.imageData =
+      'data:image/jpg;base64,' +
+      fileReader.readAsBinaryString(files[0]);
+    images.push(vehicleImage);
+    // }
+    return images;
   }
 }
