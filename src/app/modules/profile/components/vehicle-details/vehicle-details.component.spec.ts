a818f3a466d500bf1f3a253aef6d07dbf4a8d816
rmuehler <67pha0vz!!!!!>
2021-06-16 09:46:35 -0400

    added test for rendering html elements

--- src/app/modules/profile/components/vehicle-details/vehicle-details.component.spec.ts
+++ src/app/modules/profile/components/vehicle-details/vehicle-details.component.spec.ts
@@ -1,25 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VehicleDetailsComponent } from './vehicle-details.component';
+import {
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+} from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('VehicleDetailsComponent', () => {
   let component: VehicleDetailsComponent;
   let fixture: ComponentFixture<VehicleDetailsComponent>;
+  let htmlElement: any;
+  const htmlIds = [
+    '#vehicle-images',
+    '#make',
+    '#model',
+    '#year',
+    '#vin',
+    '#miles',
+    '#color',
+    '#price',
+    '#owner',
+    '#status',
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ VehicleDetailsComponent ]
-    })
-    .compileComponents();
+      declarations: [VehicleDetailsComponent],
+      imports: [RouterTestingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(VehicleDetailsComponent);
     component = fixture.componentInstance;
+    htmlElement = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render all html elements', () => {
+    htmlIds.forEach((id) =>
+      expect(htmlElement.querySelector(id).toBeTruthy()),
+    );
   });
 });
--- src/app/modules/profile/components/vehicle-details/vehicle-details.component.ts
+++ src/app/modules/profile/components/vehicle-details/vehicle-details.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-vehicle-details',
   templateUrl: './vehicle-details.component.html',
-  styleUrls: ['./vehicle-details.component.css']
+  styleUrls: ['./vehicle-details.component.css'],
 })
 export class VehicleDetailsComponent implements OnInit {
+  id: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      (data) => (this.id = data.get('id')),
+    );
   }
-
 }
